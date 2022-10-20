ed9941df7c1da3018741506387bfffa6bc5e316f
nasiabuslikprog <nasiabuslikprog@yandex.ru>
2022-10-20 11:06:24 +0300

    Variable names changed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         IndexService service = new IndexService();
-        double myIndex = service.index(68.4, 1.63);
-        System.out.println( "Индекс массы тела " + myIndex);
+        double bodyMassIndex = service.index(68.4, 1.63);
+        System.out.println( "Индекс массы тела " + bodyMassIndex);
 
     }
 
